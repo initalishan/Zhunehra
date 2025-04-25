98e61398f617f0e59466f4dc9599e2f21559e0fd
initalishan <initalishan@gmail.com>
2025-04-25 15:43:58 +0530

    fix queue system

--- zhunehra/plugins/queue.py
+++ zhunehra/plugins/queue.py
@@ -8,49 +8,71 @@ import os
 
 queue = []
 queue_position = 0
-paths = []
-paths_index = 0
+current_index = 1
+url_mention = ""
 
 async def add_to_queue(song_name, chat_id, mention):
     global queue_position
     global queue
-    if len(queue) == queue_position:
+    global url_mention
+    url_mention = mention
+    if len(queue) < 1:
         queue.append(song_name)
         path = await download(queue[queue_position], "m4a", chat_id)
         await Play_Audio(chat_id, path)
-        data = await meta_data(song_name, chat_id)
-        await zhunehra.send_file(
-                chat_id,
-                file=data[3],
-                caption=f"**{data[0]}**\n\n**Artist:** {data[1]}\n**Duration:** {data[2]}\n**Requested by:** {mention}"
-            )
+        await Call.change_call_volume(chat_id, 200)
+        os.remove(path)
+        await playing_message(song_name, chat_id)
     else:
-        data = await meta_data(song_name, chat_id)
         queue_position += 1
         queue.append(song_name)
-        await zhunehra.send_message(
-            chat_id,
-            f"**Added to queue.**\n\n{data[0]}\n\n**Artist:** {data[1]}\n**Duration:** {data[2]}\n**Queue Position:** #{queue_position}"
-        )
-        if (len(queue) -1) == queue_position:
-            path = await download(queue[queue_position], "m4a", chat_id)
-            paths.append(path)
+        await queue_message(song_name, chat_id, queue_position)
         
 async def play_next(chat_id):
     global queue
+    global current_index
     global queue_position
-    if len(queue) > queue_position and len(queue) < (1+queue_position):
-        await Play_Audio(chat_id, queue[queue_position])
-   
+    print(len(queue))
+    print(current_index)
+    if len(queue) > current_index:
+        path = await download(queue[current_index], "m4a", chat_id)
+        print(queue)
+        await Play_Audio(chat_id, path)
+        os.remove(path)
+        await playing_message(queue[current_index], chat_id)
+        current_index += 1
+        
+    else:
+        queue.clear()
+        current_index = 0
+        queue_position = 0
+        await Call.leave_call(chat_id)
+        await zhunehra.send_message(chat_id, "Queue finished, Leaving voice chat.")
+    
 @Call.on_update(filters.stream_end())
 async def stream_end(_, update: Update):
-    global paths_index
-    global paths
-    global queue
-    global queue_position
     chat_id = update.chat_id
-    path = paths[paths_index]
-    print(f"Now playing {path}")
-    await Play_Audio(chat_id, path)
-    if paths_index > (len(paths)):
-        paths_index += 1
+    await play_next(chat_id)
+        
+async def playing_message(song_name, chat_id):
+    global url_mention
+    data = await meta_data(song_name, chat_id)
+    await zhunehra.send_file(
+        chat_id,
+        file=data[3],
+        caption=f"**{data[0]}**\n\n**Artist:** {data[1]}\n**Duration:** {data[2]}\n**Requested by:** {url_mention}"
+    )
+    if data[3] == "db/zhunehra.png":
+        pass
+    else:
+        os.remove(data[3])
+async def queue_message(song_name, chat_id, queue_position):
+    data = await meta_data(song_name, chat_id)
+    await zhunehra.send_message(
+        chat_id,
+        f"**Added to queue.**\n\n{data[0]}\n\n**Artist:** {data[1]}\n**Duration:** {data[2]}\n**Queue Position:** #{queue_position}"
+    )
+    if data[3] == "db/zhunehra.png":
+        pass
+    else:
+        os.remove(data[3])
